906c9acf60d3e456e5213186fa4c46e06dfc228d
NMShihab <nmshihabislam@gmail.com>
2021-08-13 23:25:40 +0600

    add bump animation

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart_contex";
 import style_classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnAnimation, setBtnAnimation] = useState(false);
   const cartCtx = useContext(CartContext);
   const numOfCartItem = cartCtx.items.reduce((currNumber, item) => {
     return currNumber + item.amount;
   }, 0);
+
+  const btnClases = `${style_classes.button} ${
+    btnAnimation ? style_classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnAnimation(true);
+
+    const timer = setTimeout(() => {
+      setBtnAnimation(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [cartCtx.items]);
   return (
-    <button className={style_classes.button} onClick={props.onClick}>
+    <button className={btnClases} onClick={props.onClick}>
       <span className={style_classes.icon}>
         <CartIcon />
       </span>
